2039e4955b1a967a5687c87be502ca17f9d9a56d
TechMrs <kemilat50@gmail.com>
2024-01-22 17:52:18 +0800

    chore: First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss'
+import Main from './components/section-1/Main'
+import Social from './components/social/Social'
+import Advantage from './components/sectionThree/Advantage'
+
+function App() {
+
+  return (
+    <div className=''>
+      <Main />
+      <Social />
+      <Advantage />
+    </div>
+  )
+}
+
+export default App
